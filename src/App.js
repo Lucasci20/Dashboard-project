744e4cfdfcec8184a445032f3be7948cc98bac48
Lucasci20 <lucascipriano2002@gmail.com>
2022-02-09 17:35:38 -0300

    update 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Home from './pages/Home'
-import Sidebar from './components/Sidebar/Sidebar'
-import Table from './components/table'
 
 import './assets/icons/style.css'
 import './assets/styles/global.css'
